d18e421443d5c9cea943987dd2e5179c3567e967
phbice <fpie2_314@163.com>
2022-08-22 19:05:14 +0800

    修改了 PowerShell 中的脚本

--- PowerShell/hashtable.ps1
+++ PowerShell/hashtable.ps1
@@ -1,15 +1,27 @@
 ﻿# 哈希表储存键值对
 $stu = @{name = "小明"; age=12; sex="男"}
+write-host '哈希表：'
 $stu
-$stu["name"]
 
 # 可以储存列表
-$stu = @{name = "小明"; age=12; sex="男"; books="三国演义", "平凡的世界", "三体"; sports="乒乓球"}
+$stu = @{name = "小明"; age=12; sex="男";
+books="三国演义", "平凡的世界", "三体"; sports="乒乓球"}
+write-host '哈希表-包含列表：'
 $stu
 
 # 键值的增删查改
-$stu.age = 20
+# 增加
 $stu.English = "CET-6"
+$stu["ID"] = "1234567"
+$stu.Add("married", $false)
+# 删除
 $stu.Remove("sprots")
+# 查找
+write-host('查找键值：')
 $stu.name
+$stu["name"]
+# 修改
+$stu.age = 20
+
+write-host '增删查改后的结果：'
 $stu
--- PowerShell/return_value.ps1
+++ PowerShell/return_value.ps1
@@ -1,5 +1,5 @@
 ﻿# 命令的返回值为.net对象，若返回值包含多个对象则以数组返回
 
-$dirlist = ls
+$dirlist = get-childitem
 $dirlist.Count
-$dirlist[0] | fl * # fl = Format-List
+$dirlist[0] | format-list *
